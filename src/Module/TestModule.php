2a4dbabc31ce34682441f9e3e997d3defe42cb22
pazworld <paz@moudamepo.net>
2022-12-23 20:14:32 +0900

    prepare for test

---
+++ src/Module/TestModule.php
@@ -0,0 +1,17 @@
+<?php
+  
+declare(strict_types=1);
+
+namespace MyVendor\HtmxMemo\Module;
+
+use BEAR\Package\AbstractAppModule;
+use Ray\AuraSqlModule\AuraSqlModule;
+
+class TestModule extends AbstractAppModule
+{
+    protected function configure(): void
+    {   
+        $dbConfig = 'sqlite::memory:';
+        $this->install(new AuraSqlModule($dbConfig));
+    }
+}
