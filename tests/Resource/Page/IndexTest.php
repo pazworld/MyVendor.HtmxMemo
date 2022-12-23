608a24a9b05df106b8bb74c3fdbe38e735e0506a
pazworld <paz@moudamepo.net>
2022-12-23 20:14:56 +0900

    add test for page/index

--- tests/Resource/Page/IndexTest.php
+++ tests/Resource/Page/IndexTest.php
@@ -4,24 +4,30 @@ declare(strict_types=1);
 
 namespace MyVendor\HtmxMemo\Resource\Page;
 
+use Aura\Sql\ExtendedPdoInterface;
 use BEAR\Resource\ResourceInterface;
 use MyVendor\HtmxMemo\Injector;
 use PHPUnit\Framework\TestCase;
 
 class IndexTest extends TestCase
 {
+    private ExtendedPdoInterface $pdo;
     private ResourceInterface $resource;
 
     protected function setUp(): void
     {
-        $injector = Injector::getInstance('app');
+        $injector = Injector::getInstance('test-html-app');
         $this->resource = $injector->getInstance(ResourceInterface::class);
+        $this->pdo = $injector->getInstance(ExtendedPdoInterface::class);
+        $this->pdo->perform('DROP TABLE IF EXISTS memos');
+        $this->pdo->perform('CREATE TABLE memos (id INTEGER PRIMARY KEY, title TEXT)');
+        $this->pdo->perform('INSERT INTO memos (title) VALUES ("ねずみ")');
     }
 
     public function testOnGet(): void
     {
-        $ro = $this->resource->get('page://self/index', ['name' => 'BEAR.Sunday']);
+        $ro = $this->resource->get('page://self/index');
         $this->assertSame(200, $ro->code);
-        $this->assertSame('Hello BEAR.Sunday', $ro->body['greeting']);
+        $this->assertMatchesRegularExpression('/ねずみ/', (string) $ro);
     }
 }
