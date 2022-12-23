7d88ee299e3cbb101cc87164ac5161a613cbd024
pazworld <paz@moudamepo.net>
2022-12-23 18:46:59 +0900

    setup AuraSqlModule

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -7,6 +7,7 @@ namespace MyVendor\HtmxMemo\Module;
 use BEAR\Dotenv\Dotenv;
 use BEAR\Package\AbstractAppModule;
 use BEAR\Package\PackageModule;
+use Ray\AuraSqlModule\AuraSqlModule;
 
 use function dirname;
 
@@ -15,6 +16,13 @@ class AppModule extends AbstractAppModule
     protected function configure(): void
     {
         (new Dotenv())->load(dirname(__DIR__, 2));
-        $this->install(new PackageModule());
+        $dbpath = dirname(__DIR__) . '/../var/db/database.sqlite3';
+        $this->install(
+            new AuraSqlModule(
+                'sqlite:' . $dbpath
+                )
+            );
+            $this->install(new PackageModule());
+        }
     }
-}
+    
