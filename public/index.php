172430fc256216eb6b4bf834070a82b9628ea29b
pazworld <paz@moudamepo.net>
2022-12-26 18:47:55 +0900

    omit hal context

--- public/index.php
+++ public/index.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 use MyVendor\HtmxMemo\Bootstrap;
 
 require dirname(__DIR__) . '/autoload.php';
-exit((new Bootstrap())(PHP_SAPI === 'cli-server' ? 'html-hal-app' : 'prod-html-hal-app', $GLOBALS, $_SERVER));
+exit((new Bootstrap())(PHP_SAPI === 'cli-server' ? 'html-app' : 'prod-html-app', $GLOBALS, $_SERVER));
