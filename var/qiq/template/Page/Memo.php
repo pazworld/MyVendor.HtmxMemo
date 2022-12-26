e115528a0a2b7c342d4ab2f7be1fb7635f0c5071
pazworld <paz@moudamepo.net>
2022-12-26 17:54:56 +0900

    modify for delete

--- var/qiq/template/Page/Memo.php
+++ var/qiq/template/Page/Memo.php
@@ -2,10 +2,13 @@
     {{ $id = (string) $this->memo['id'] }}
     {{ $title = $this->memo['title'] }}
     <tr hx-target="this" hx-swap="outerHTML">
-        <td>{{h $title }}</td>
+        <td>
+            <input type="hidden" name="id" value="{{h $id }}"/>
+            {{h $title }}
+        </td>
         <td class="td-button">
             <button hx-get="/edit-memo?id={{h $id }}" class="button is-warning">編集</button>
-            <button class="button is-danger">削除</button>
+            <button hx-delete="/memo" hx-include="closest tr" class="button is-danger" hx-swap="outerHTML swap:1s">削除</button>
         </td>
     </tr>
 {{ endif }}
